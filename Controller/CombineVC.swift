a896bd637143d2ab755bfc36c71e74ba1fe57ace
bielaragaoo <97034610+bielaragaoo@users.noreply.github.com>
2022-04-10 17:05:21 -0300

    feat(AppTinder): Animaçao do Card

---
+++ Controller/CombineVC.swift
@@ -0,0 +1,69 @@
+//
+//  CombineVC.swift
+//  AppTinder
+//
+//  Created by Gabriel Aragao on 08/04/22.
+//
+
+import UIKit
+
+class CombineVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.systemGroupedBackground
+        
+        self.adicionarCards()
+             
+    }
+}
+
+extension CombineVC {
+    func adicionarCards () {
+        
+        for item in 1...3 {
+        
+        let redView = UIView()
+            redView.backgroundColor = item == 2 ? .blue : .red
+        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        
+        redView.center = view.center
+        
+        let gesture = UIPanGestureRecognizer()
+        
+        gesture.addTarget(self, action: #selector(handlerCard))
+        
+        redView.addGestureRecognizer(gesture)
+        view.addSubview(redView)
+        
+        }
+        
+    }
+}
+
+
+extension CombineVC {
+    @objc func handlerCard (gesture: UIPanGestureRecognizer){
+        
+        if let card = gesture.view {
+            let point = gesture.translation(in: view)
+
+            card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+            
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+            
+            if gesture.state == .ended {
+                
+                UIView.animate(withDuration: 0.25){
+                    card.center = self.view.center
+                    card.transform = .identity
+                }
+                
+            }
+        }
+        
+    }
+}
